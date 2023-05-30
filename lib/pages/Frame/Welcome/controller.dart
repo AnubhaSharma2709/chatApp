f3f8835aa44f3f447ac2d18b923076b850c45d8e
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-05-30 20:46:35 +0530

    commit

---
+++ lib/pages/Frame/Welcome/controller.dart
@@ -0,0 +1,13 @@
+import 'package:chatapp/pages/Frame/Welcome/state.dart';
+import 'package:get/get.dart';
+
+class WelcomeController extends GetxController{
+  WelcomeController();
+  final title = "ChatApp";
+  final state = WelcomeState();
+
+  @override
+  void onReady(){
+
+  }
+}
